9b70befc06b9be13dda4c8cae907c209f6680c79
Andrey Bagryanskiy <andrey@1dist.ru>
2023-10-16 00:17:45 +0300

    111

--- main.cpp
+++ main.cpp
@@ -23,8 +23,38 @@ void addToStock(std::map<std::string, int>& stock){
     }
 }
 
+void mapOut(const std::map<std::string, int>& outMap){
+    for(auto it = outMap.begin(); it != outMap.end(); ++it){
+        std::cout << it->first << " " << it->second << std::endl;
+    }
+}
+
+void addToCart(std::map<std::string, int>& stock, std::map<std::string, int>& cart){
+    std::string article;
+    std::cout << "Input the article to add to the cart: ";
+    std::cin >> article;
+    if(stock.find(article) == stock.end()){
+        throw std::invalid_argument("The article was not found in the store!");
+    }
+    if(stock.find(article)->second <= 0){
+        throw std::invalid_argument("The product ran out in the store!");
+    }
+    int quantity;
+
+
+}
+
+
+
 int main(){
     std::map<std::string, int> stock;
     std::map<std::string, int> cart;
     addToStock(stock);
+    mapOut(stock);
+    try{
+        addToCart(stock, cart);
+    }catch (const std::invalid_argument& x){
+        std::cerr << "Invalid argument supplied: " << x.what() << std::endl;
+    }
+
 }
